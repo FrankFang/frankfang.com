0c91df4d4063e7a535e02e80a155fa387ee7674b
frankfang <frankfang1990@gmail.com>
2017-02-13 21:33:31 +0800

    update styles

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ items = document.querySelectorAll('x-markdown')
 Array.prototype.forEach.call(items, (item)=>{
   html = marked(item.textContent)
   item.innerHTML = html
+  item.classList.add('converted')
 })
 
 
